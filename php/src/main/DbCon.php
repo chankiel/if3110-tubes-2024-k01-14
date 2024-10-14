dd5a765df943f330273b89d24205002bf624e191
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-10-14 18:02:57 +0700

    feat: add essentials db abstraction

--- php/src/main/DbCon.php
+++ php/src/main/DbCon.php
@@ -10,7 +10,7 @@ class DbCon {
     private $password;
     private $db;
     private $port;
-    private $pdo;
+    private PDO $pdo;
 
     private static $instance = null;
 
@@ -44,11 +44,130 @@ class DbCon {
         }
     }
 
+    // Langsung query mentah, jangan untuk input user
+    // Cth: query("SELECT * FROM USERS")
     public function query($sql) {
         $result = $this->pdo->query($sql)->fetchAll();
-        if (!$result) {
-            die("Error query: " . $this->pdo->errorInfo());
-        }
         return $result;
+    }
+
+    /*
+        prepareQuery(
+         "SELECT * FROM :table WHERE id = :id",
+         ['id'=>5]
+        )
+    */
+    // Query dengan input user
+    public function prepareQuery($sql, $params=[]) {
+        try{
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute($params);
+            return $stmt->fetchAll();
+        }catch(PDOException $e){
+            die("Error prepare query :".$e->getMessage());
+        }
+    }
+
+    /*
+        insert(
+         "users",
+         ['name'=>"labpro"]
+        )
+    */
+    public function insert($table,$data){
+        $columns = implode(", ",array_keys($data));
+        $placeholders = ":". implode(", :",array_keys($data));
+
+        $sql = "INSERT INTO $table ($columns) VALUES ($placeholders)";
+
+        try{
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute($data);
+            return $this->pdo->lastInsertId();
+        }catch(PDOException $e){
+            die("Error insert: ".$e->getMessage());
+        }
+    }
+
+    /* 
+        update(
+         "users",
+         ['name'=>'labpro'],
+         "email=:email',
+         ['email'=>'a@gmail.com']
+        )
+    */
+    public function update($table, $data, $condition, $params = []) {
+        $fields = "";
+        foreach ($data as $key => $value) {
+            $fields .= "$key = :new_$key, ";
+        }
+        $fields = rtrim($fields, ", "); 
+    
+        $sql = "UPDATE $table SET $fields WHERE $condition";
+    
+        try {
+            $stmt = $this->pdo->prepare($sql);
+            foreach ($data as $key => $value) {
+                $stmt->bindValue(":new_$key", $value);
+            }
+            foreach ($params as $key => $value) {
+                $stmt->bindValue(":$key", $value); 
+            }
+            $stmt->execute();    
+            return $stmt->rowCount();
+    
+        } catch (PDOException $e) {
+            die("Error updating data: " . $e->getMessage());
+        }
+    }
+    
+    /*
+        delete(
+         "users",
+         "id = :id",
+         ["id"=>1],
+        )
+    */
+    public function delete($table, $condition, $params=[]){
+        $sql = "DELETE FROM $table WHERE $condition";
+        try{
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute($params);
+            return $stmt->rowCount();
+        }catch(PDOException $e){
+            die("Error delete: ".$e->getMessage());
+        }
+    }
+
+    /*
+        findById(
+         "users",
+         1,
+        )
+    */
+    public function findById($table, $id) {
+        $sql = "SELECT * FROM $table WHERE id = :id";
+        
+        try {
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute(['id' => $id]);
+            return $stmt->fetch();
+        } catch (PDOException $e) {
+            die("Error fetching data: " . $e->getMessage());
+        }
+    }
+    
+
+    public function beginTransaction() {
+        $this->pdo->beginTransaction();
+    }
+    
+    public function commit() {
+        $this->pdo->commit();
+    }
+    
+    public function rollBack() {
+        $this->pdo->rollBack();
     }
 }
