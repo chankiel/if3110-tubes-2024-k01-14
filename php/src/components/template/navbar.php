77ffdd61e5e1975740a99f71bb8c8bc7fdcfe3c6
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-10-15 18:00:12 +0700

    feat: add navbar (no func and auth)

--- php/src/components/template/navbar.php
+++ php/src/components/template/navbar.php
@@ -1,11 +1,22 @@
+<header class="">
+    <div class="logo-container">
+        <img src="/public/images/linkedin.png" alt="logo">
+    </div>
+    <nav class="navbar ">
+        <ul class="navbar-list ">
+            <li class="home">
+                <span class="material-symbols-outlined navbar-icon">
+                    home
+                </span>
+                <a href="/">Home</a>
+            </li>
+            <li><a href="/login" class="login-btn">Login</a></li>
+            <li><a href="/register" class="register-btn">Register</a></li>
+            <!-- <li class="home">
+                <img src="/public/images/linkedin.png" alt="logo">
+                <a href="/" class="name-link">Ignatius</a>
+            </li> -->
 
-<header class="header">
-    <nav class="navbar">
-        <ul class="navbar-list">
-            <li><a href="/">Home</a></li>
-
-            <li><a href="">Login</a></li>
-            <li><a href="">Register</a></li>
         </ul>
     </nav>
 </header>
