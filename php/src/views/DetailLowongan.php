ee0e33cebe13970c4baba94eb648608c8b5b456f
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-10-17 21:58:38 +0700

    feat: add conditional-rendering

--- php/src/views/DetailLowongan.php
+++ php/src/views/DetailLowongan.php
@@ -6,19 +6,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
-    <link rel="stylesheet" href="../public/styles/style.css">
-    <link rel="stylesheet" href="../public/styles/template/navbar.css">
-    <link rel="stylesheet" href="../public/styles/company/DetailLowongan.css">
+    <link rel="stylesheet" href="/public/styles/style.css">
+    <link rel="stylesheet" href="/public/styles/template/navbar.css">
+    <link rel="stylesheet" href="/public/styles/company/DetailLowongan.css">
 </head>
 
 <body>
     <?php include(dirname(__DIR__) . '/components/template/navbar.php') ?>
     <main>
         <section>
-            <p>Agoda</p>
-            <h1 class="position-heading">
-                Senior Software Engineer
-            </h1>
+            <div class="heading-container">
+                <button id="back-btn" class="material-symbols-outlined">
+                    arrow_back
+                </button>
+                <p>Agoda</p>
+                <h1 class="position-heading">
+                    Senior Software Engineer
+                </h1>
+            </div>
             <p class="small-details">Bandung, Jawa Barat, Indonesia · Diposting 2 minggu lalu</p>
             <ul class="lowongan-details details-format">
                 <li>
@@ -39,43 +44,48 @@
                     </span>
                     <p>On-Site</p>
                 </li>
+                <?php if (true): ?>
+                    <button class="general-btn lamar-btn">
+                        Lamar
+                    </button>
             </ul>
-            <!-- <button class="general-btn lamar-btn">
-                Lamar
-            </button> -->
-            <button class="general-btn applied-btn">
-                <span class="material-symbols-outlined">
-                    check
-                </span>
-                <p>
-                    Applied
-                </p>
-            </button>
-            <div class="applied-details">
-                <div class="applied-status">
-                    <h2 class="applied-detail-heading">Status: <span class="status">Accepted</span></h2>
-                    <p>Selamat! Anda diterima karena ganteng! Silahkan menghubungi saya di 0822124124142</p>
-                    <h2 class="applied-detail-heading">Attachments:</h2>
-                    <ul class="details-format attachment-list">
-                        <li>
-                            <span class="material-symbols-outlined">
-                                videocam
-                            </span>
-                            <p>KielPerkenalan.mp4</p>
-                        </li>
-                        <li>
-                            <span class="material-symbols-outlined">
-                                description
-                            </span>
-                            <p>KielCV.pdf</p>
-                        </li>
-                    </ul>
+            <?php else: ?>
+                <button class="general-btn applied-btn">
+                    <span class="material-symbols-outlined">
+                        check
+                    </span>
+                    <p>
+                        Applied
+                    </p>
+                </button>
+                <div class="applied-details">
+                    <div class="applied-status">
+                        <h2 class="applied-detail-heading">Status: <span class="status">Accepted</span></h2>
+                        <p>Selamat! Anda diterima karena ganteng! Silahkan menghubungi saya di 0822124124142</p>
+                        <h2 class="applied-detail-heading">Attachments:</h2>
+                        <ul class="details-format attachment-list">
+                            <li>
+                                <span class="material-symbols-outlined">
+                                    videocam
+                                </span>
+                                <p>KielPerkenalan.mp4</p>
+                            </li>
+                            <li>
+                                <span class="material-symbols-outlined">
+                                    description
+                                </span>
+                                <p>KielCV.pdf</p>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
+            <?php endif ?>
         </section>
         <section>
             <h1 class="lowongan-heading">Tentang Agoda</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore suscipit ut, nobis enim aliquam laudantium aut magnam fugiat, error iure laborum temporibus dolorem, eveniet impedit dignissimos! Dolorem atque ad repudiandae?</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore suscipit ut, nobis enim aliquam laudantium aut magnam fugiat, error iure laborum temporibus dolorem, eveniet impedit dignissimos! Dolorem atque ad repudiandae? <br>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae illum magnam placeat? Corrupti maiores cumque impedit praesentium, inventore, a doloremque porro molestiae molestias aut earum itaque reprehenderit at laudantium eius?
+            </p>
         </section>
         <section>
             <h1 class="lowongan-heading">Tentang Pekerjaan Ini</h1>
