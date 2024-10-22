7e611f9c6babb544cc6d2d75d0fa3cdbc82093ff
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-10-22 15:31:02 +0700

    feat: modify parameter

--- php/src/Helper/FileManager.php
+++ php/src/Helper/FileManager.php
@@ -4,16 +4,16 @@ namespace Helper;
 
 class FileManager
 {
-    public static function uploadFile($type)
+    public static function getAndUploadFile($dir,$type)
     {
-        $upload_dir = dirname(__DIR__) . '/storage/'. $type;
+        $upload_dir = dirname(__DIR__) . $dir . $type;
 
         if(!isset($_FILES[$type])){
             return null;
         }
 
         if (!is_dir($upload_dir)) {
-            mkdir($upload_dir, 0755, true);  // Create the directory with permissions and recursive flag
+            mkdir($upload_dir, 0755, true); 
         }
 
         $file = $_FILES[$type]['tmp_name'];
@@ -22,12 +22,44 @@ class FileManager
 
         if (!empty($file)) {
             if (move_uploaded_file($file, $target)) {
-                return "/storage/".$type.'/'.$filename;
+                return $dir.$type.'/'.$filename;
             } else {
                 return null;
             }
         }else{
             return null;
         }
+    }
+
+    public static function uploadFile($dir,$type, $fileTmpName, $fileName)
+    {
+        $upload_dir = dirname(__DIR__) . $dir . $type;
+
+        if (!is_dir($upload_dir)) {
+            mkdir($upload_dir, 0755, true);  
+        }
+
+        $target = $upload_dir . '/' . basename($fileName);
+
+        if (!empty($fileTmpName)) {
+            if (move_uploaded_file($fileTmpName, $target)) {
+                return $dir.$type.'/'.basename($fileName);
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    public static function deleteFile($file_path){
+        $file_path = ltrim($file_path,'/');
+        $absPath = dirname(__DIR__) . '/' . $file_path;
+        if(file_exists($absPath)){
+            if(unlink($absPath)){
+                return true;
+            }
+        }
+        return false;
     }
 }
