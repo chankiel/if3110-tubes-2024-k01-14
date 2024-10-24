f06e93315c2857ca523bb8ee270840c88fd38138
Francesco Michael Kusuma <francescokusuma15@gmail.com>
2024-10-24 19:56:47 +0700

    fix : sync search

--- php/src/components/home/searchBox.php
+++ php/src/components/home/searchBox.php
@@ -2,7 +2,7 @@
     <span class="material-symbols-outlined" data-icon="search">
         search
     </span>
-    <input id="job-search" type="text" placeholder="Search">
+    <input id="job-search" class = "job-search"type="text" placeholder="Search">
     <span class="material-symbols-outlined toggle-filter" data-icon="filter">
         filter_alt
     </span>
