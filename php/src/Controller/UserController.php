e804f8a33ed4a2508f0e6370d0ba1850768a4bfd
Francesco Michael Kusuma <francescokusuma15@gmail.com>
2024-10-26 23:03:09 +0700

    feat: no recom and applicant

--- php/src/Controller/UserController.php
+++ php/src/Controller/UserController.php
@@ -26,6 +26,7 @@ class UserController extends Controller
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
         }
+        
         if ($this->user->userExists($_POST["email"]) || $this->user->userExists($_POST["email_company"])) {
             $_SESSION["error_message"] = "Email is already registered.";
             header('Location: /register');
